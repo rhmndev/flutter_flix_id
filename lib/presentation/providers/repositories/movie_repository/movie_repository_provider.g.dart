76019035efcd788f2bb84242d60c66d6ea337e77
Rahman Nurhadi <89194617+rhmndev@users.noreply.github.com>
2025-07-11 08:56:24 +0700

    feat: add provider for transaction_repository and movie_repository

--- lib/presentation/providers/repositories/movie_repository/movie_repository_provider.g.dart
+++ lib/presentation/providers/repositories/movie_repository/movie_repository_provider.g.dart
@@ -1,28 +1,28 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'user_repository_provider.dart';
+part of 'movie_repository_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$userRepositoryHash() => r'1a3a9f80ec27bfb78bd4ac37319076992ed8dbf4';
+String _$movieRepositoryHash() => r'946c5774e7751b537d79db5545fcbc1912191144';
 
-/// See also [userRepository].
-@ProviderFor(userRepository)
-final userRepositoryProvider = AutoDisposeProvider<UserRepository>.internal(
-  userRepository,
-  name: r'userRepositoryProvider',
+/// See also [movieRepository].
+@ProviderFor(movieRepository)
+final movieRepositoryProvider = AutoDisposeProvider<MovieRepository>.internal(
+  movieRepository,
+  name: r'movieRepositoryProvider',
   debugGetCreateSourceHash:
       const bool.fromEnvironment('dart.vm.product')
           ? null
-          : _$userRepositoryHash,
+          : _$movieRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef UserRepositoryRef = AutoDisposeProviderRef<UserRepository>;
+typedef MovieRepositoryRef = AutoDisposeProviderRef<MovieRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/presentation/providers/repositories/transaction_repository/transaction_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_flix_id/data/firebase/firebase_transaction_repository.dart';
+import 'package:flutter_flix_id/data/repositories/transaction_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'transaction_repository_provider.g.dart';
+
+@riverpod
+TransactionRepository transactionRepository(TransactionRepositoryRef ref) =>
+    FirebaseTransactionRepository();
